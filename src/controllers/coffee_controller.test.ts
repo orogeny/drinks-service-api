2ebac587ac448ad1710ae4d9e6211546dc763076
Paul <paulwharin@gmail.com>
2023-11-23 15:36:05 +0000

    test: add test for different coffee styles

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -2,15 +2,23 @@ import request from "supertest";
 import { app } from "../app";
 
 describe("/coffee endpoint", () => {
-  test("GET /coffee should return correct object", async () => {
-    const res = await request(app)
-      .get("/coffee")
-      .query({ coffeeName: "Latte" });
+  test("GET /coffee should return default object", async () => {
+    const res = await request(app).get("/coffee");
 
     expect(res.statusCode).toEqual(200);
     expect(res.body).toEqual({
       drinkType: "Coffee",
       name: "Latte",
+    });
+  });
+
+  test("GET /coffee returns requested coffee name", async () => {
+    const res = await request(app).get("/coffee?coffeeName=Espresso");
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Espresso",
     });
   });
 });
